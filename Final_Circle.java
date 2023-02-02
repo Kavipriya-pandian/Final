530d01c95f908d866852286935e2b80b20152088
Kavipriya-pandian <120665691+Kavipriya-pandian@users.noreply.github.com>
2023-02-02 20:54:24 +0530

    Add files via upload

---
+++ Final_Circle.java
@@ -0,0 +1,67 @@
+package dxc;
+class Circle
+{
+	private final double PI;
+	private double diameter;
+	private double  circumference;
+	private double area;
+	public Circle(double diameter) 
+	{
+		this.PI = 3.14;
+		this.diameter = diameter;
+	}
+	public void calculateCircumfernce()
+	{
+		circumference=PI*diameter;
+	}
+	public void calculateArea()
+	{
+		double radius=diameter/2;
+		area=PI*radius*radius;
+	}
+	public double getDiameter() {
+		return diameter;
+	}
+	public void setDiameter(double diameter) {
+		this.diameter = diameter;
+	}
+	public double getCircumference() {
+		return circumference;
+	}
+	public void setCircumference(double circumference) {
+		this.circumference = circumference;
+	}
+	public double getArea() {
+		return area;
+	}
+	public void setArea(double area) {
+		this.area = area;
+	}
+	public double getPI() {
+		return PI;
+	}
+	
+}
+
+public class Final_Circle
+{
+
+	public static void main(String[] args)
+	{
+		Circle circle1 = new Circle(10.2);
+		 Circle circle2 = new Circle(5.7);
+		 //Create more objects of Circle class and add to the array given below for testing your code 
+		 Circle[] circles = {circle1, circle2};
+		 for (Circle circle : circles)
+		 {
+		 circle.calculateCircumfernce();
+		 circle.calculateArea();
+		 System.out.println("Diameter of the circle is "+circle.getDiameter());
+		 System.out.println("Circumference of the circle is " + 
+		Math.round(circle.getCircumference()*100)/100.0);
+		 System.out.println("Area of the circle is " + Math.round(circle.getArea()*100)/100.0);
+		 System.out.println();
+		 }
+	}
+
+}
